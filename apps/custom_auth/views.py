1b1885630bfba678688d2d5b8cde817c71136bbd
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-16 12:19:04 -0500

    feat: Add API views and serializers for custom_auth app

--- apps/custom_auth/views.py
+++ apps/custom_auth/views.py
@@ -6,6 +6,75 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.parsers import MultiPartParser, FormParser
 from .models import User
 from .serializers import *
+from rest_framework import generics, permissions
+from .models import ProfileInformation
+from .serializers import ProfileInformationSerializer
+from rest_framework.exceptions import PermissionDenied
+
+
+class PostCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        user_id = self.request.query_params.get('user_id')
+        if user_id:
+            return Post.objects.filter(user_id=user_id).order_by('-created_at')
+        return Post.objects.none()
+
+
+class PostDeleteView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+
+class ProfileInformationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProfileInformation.objects.all()
+    serializer_class = ProfileInformationSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_object(self):
+        profile_information, created = ProfileInformation.objects.get_or_create(
+            user=self.request.user)
+        return profile_information
+
+    def perform_update(self, serializer):
+        if self.request.user.id != serializer.instance.user.id:
+            raise PermissionDenied(
+                "You do not have permission to edit this profile.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if self.request.user.id != instance.user.id:
+            raise PermissionDenied(
+                "You do not have permission to delete this profile.")
+        if not instance.about_me and not instance.disciplines and not instance.contact_info:
+            instance.delete()
+        else:
+            raise ValidationError(
+                "Profile information must be empty to be deleted.")
+
+
+class PublicProfileInformationDetailView(generics.RetrieveAPIView):
+    queryset = ProfileInformation.objects.all()
+    serializer_class = ProfileInformationSerializer
+    permission_classes = [permissions.AllowAny]
+    lookup_field = 'user__id'
 
 
 class GroupListCreateView(generics.ListCreateAPIView):
