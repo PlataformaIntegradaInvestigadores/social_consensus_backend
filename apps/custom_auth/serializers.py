a8cb76ec2f5dffdf16f2596f286eda655fd2d4c1
Danny Cabrera <danny.cabrera@epn.edu.ec>
2024-06-16 12:18:50 -0500

    feat: Add serializers for PostFile and ProfileInformation models
    This commit adds serializers for the `PostFile` and `ProfileInformation` models in the `custom_auth` app. The `PostFileSerializer` is used to serialize the `file` field of the `PostFile` model, while the `ProfileInformationSerializer` is used to serialize the `about_me`, `disciplines`, and `contact_info` fields of the `ProfileInformation` model. This commit message follows the established convention of starting with a verb in the imperative mood and providing a concise summary of the changes made.

--- apps/custom_auth/serializers.py
+++ apps/custom_auth/serializers.py
@@ -1,8 +1,42 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
-from .models import User, Group
+from .models import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class PostFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostFile
+        fields = ['file']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    files = PostFileSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'description', 'files', 'created_at']  # Include 'id'
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
+
+        if 'user' in validated_data:
+            validated_data.pop('user')
+
+        files = request.FILES.getlist('files', [])
+        post = Post.objects.create(user=user, **validated_data)
+        for file in files:
+            PostFile.objects.create(post=post, file=file)
+        return post
+
+
+class ProfileInformationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfileInformation
+        fields = ['about_me', 'disciplines', 'contact_info']
+
 
 class GroupSerializer(serializers.ModelSerializer):
     users = serializers.PrimaryKeyRelatedField(
