e3db7508249ec733fb875d6f509320dd06c999e2
JorgeCordero666 <jorge666.cordero@gmail.com>
2024-12-12 21:29:26 -0500

    Comenzar de cero

--- apps/concensus/models.py
+++ apps/concensus/models.py
@@ -8,14 +8,5 @@ from apps.concensus.domain.entities.final_topic_order import FinalTopicOrder
 from apps.concensus.domain.entities.user_phase import UserPhase
 from apps.concensus.domain.entities.result_concensus import ConsensusResult
 from apps.concensus.domain.entities.user_satisfaction import UserSatisfaction
-#  CHAT GRUPAL
-from apps.concensus.domain.entities.debate import Debate
-from apps.concensus.domain.entities.participant import DebateParticipant
-from apps.concensus.domain.entities.message import DebateMessage
-from apps.concensus.domain.entities.vote import ClassificationVote
-from apps.concensus.domain.entities.reaction import MessageReaction
-from apps.concensus.domain.entities.thread import MessageThread
-from apps.concensus.domain.entities.summary import DebateSummary, SummaryMessage
-from apps.concensus.domain.entities.important_message import ImportantMessage
 
 # Create your models here.
