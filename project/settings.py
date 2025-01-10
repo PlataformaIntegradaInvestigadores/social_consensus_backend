1c26a39c08a5cf8b1d43873d0836b1ea7d3061e5
Mateo <vimofama@outlook.com>
2025-01-10 07:33:42 -0500

    Add CORS settings to allow connections from localhost on port 80

--- project/settings.py
+++ project/settings.py
@@ -73,6 +73,9 @@ CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'http://127.0.0.1:4200',
+    'http://localhost:80',
+    'http://127.0.0.1:80',
+    'http://localhost',
 ]
 
 AUTHENTICATION_BACKENDS = (
