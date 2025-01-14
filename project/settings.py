255b8393021e936f188d35d1ca7a18bc9270bc10
Mateo <vimofama@outlook.com>
2025-01-14 18:31:04 -0500

    Actualizar las URLs permitidas en CORS para incluir el nuevo puerto y un dominio adicional

--- project/settings.py
+++ project/settings.py
@@ -74,9 +74,9 @@ CORS_ALLOW_CREDENTIALS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'http://127.0.0.1:4200',
-    'http://localhost:80',
-    'http://127.0.0.1:80',
-    'http://localhost',
+    'http://localhost:8082',
+    'http://127.0.0.1:8082',
+    'https://centinela.epn.edu.ec',
 ]
 
 AUTHENTICATION_BACKENDS = (
